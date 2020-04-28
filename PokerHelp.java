8e1bb2747c10c9787d2da1b74eb1829d641833cc
MMel099 <63422396+MMel099@users.noreply.github.com>
2020-04-28 12:28:36 -0400

    input validation

--- PokerHelp.java
+++ PokerHelp.java
@@ -10,7 +10,7 @@ public class PokerHelp{
 	ArrayList<Card> deck;
 	Hand hand;
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws fileNotFoundException{
 		Scanner filereader = new Scanner(new File "Deck");
 		while (filereader.hasNextLine()){
 			deck.add(Card(filereader.next(),filereader.next()));
@@ -25,23 +25,52 @@ public class PokerHelp{
 
 	public static ArrayList<Card> informationRetriever(){
 		ArrayList<Card> cards = new ArrayList<Card>();
+		System.out.println("Welcome to our poker win percentage calculator!")
+		System.out.println("Please input all cards as 'suit' 'value' (Examples: Ace of spads ~ spade a, Six of hearts ~ heart 6, Queen of diamonds ~ diamond q");
 		Scanner input = new Scanner(System.in);
 		System.out.println("Personal Card 1: ");
-		cards.add(card(input.next(), input.next()));
+		cards.add(validateInput(input));
 		System.out.println("Personal Card 2: ");
-		cards.add(card(input.next(), input.next()));
+		cards.add(validateInput(input));
 		System.out.println("Community Card 1 (Flop 1): ");
-		cards.add(card(input.next(), input.next()));
+		cards.add(validateInput(input));
 		System.out.println("Community Card 2 (Flop 2): ");
-		cards.add(card(input.next(), input.next()));
+		cards.add(validateInput(input));
 		System.out.println("Community Card 3 (Flop 3): ");
-		cards.add(card(input.next(), input.next()));
+		cards.add(validateInput(input));
 		System.out.println("Community Card 4 (Turn): ");
-		cards.add(card(input.next(), input.next()));
+		cards.add(validateInput(input));
 		System.out.println("Community Card 5 (River): ");
-		cards.add(card(input.next(), input.next()));
+		cards.add(validateInput(input));
 		return cards;
 	}
+
+	public static Card validateInput(Scanner input){
+		while (true){
+			String suit = input.next().toLowerCase();
+			if (suit.equals("spade") || suit.equals("diamond") || suit.equals("heart") || suit.equals("club")){
+				break;
+			}
+			System.out.println("Invalid suit input, please try again");
+		}
+		while (true){
+			try{
+				Int num = input.nextInt();
+				if (num > 1 && num < 11){
+					break;
+				}
+			}
+			try{
+				String num = input.next();
+				if (num.equals("j") || num.equals("q") || num.equals("k") || num.equals("a")){
+					break;
+				}
+			}
+			System.out.println("Invalid value input, please try again")
+		}
+		Card card = new Card(suit,num);
+		return card;
+	} 
 
 	public static void calculator(Hand hand){
 		int handsBeaten = 0;
